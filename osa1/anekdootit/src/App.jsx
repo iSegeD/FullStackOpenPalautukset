ac4bfaea2aed01a0feea0f1e43cca4b1211ed580
Sergey Dolzhenko <sergei.dolzhenko@gmail.com>
2025-03-11 14:50:05 +0200

    osa1: kurssitiedot, unicafe, anekdootit

---
+++ osa1/anekdootit/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+
+const Button = ({ onClick, text, style }) => (
+  <button onClick={onClick} style={style}>
+    {text}
+  </button>
+);
+
+const AnecdoteDisplay = ({ anecdotes, selected, votes }) => {
+  return (
+    <>
+      <p>{anecdotes[selected]}</p>
+      <p>has: {votes[selected]} votes</p>
+    </>
+  );
+};
+
+function App() {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(8).fill(0));
+
+  const handleRandomNum = () => {
+    const random = Math.floor(Math.random() * 8);
+    setSelected(random);
+  };
+
+  const handleVotes = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  };
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const mostVoted = votes.indexOf(Math.max(...votes));
+
+  /* Minun pieni lisäykseni, se näyttää suosituimmat akejdootit vain, jos ainakin yhtä niistä on äänestetty. */
+  const hasVoted = votes.some((vote) => vote > 0);
+
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <AnecdoteDisplay
+        anecdotes={anecdotes}
+        selected={selected}
+        votes={votes}
+      />
+      <Button
+        onClick={handleVotes}
+        text="Vote"
+        style={{ marginRight: "5px" }}
+      />
+      <Button onClick={handleRandomNum} text="Next Anecdote" />
+      <h2>Anecdote with most votes</h2>
+      {hasVoted && (
+        <AnecdoteDisplay
+          anecdotes={anecdotes}
+          selected={mostVoted}
+          votes={votes}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}:</td>
+      <td>{text === "Positive" ? `${value} %` : value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all === 0 ? 0 : (good - bad) / all;
+  const positive = all === 0 ? 0 : (good / all) * 100;
+
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="Good" value={good} />
+        <StatisticLine text="Neutral" value={neutral} />
+        <StatisticLine text="Bad" value={bad} />
+        <StatisticLine text="All" value={all} />
+        <StatisticLine text="Average" value={average} />
+        <StatisticLine text="Positive" value={positive} />
+      </tbody>
+    </table>
+  );
+};
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h2>Give Feedback</h2>
+      <Button onClick={() => setGood(good + 1)} text="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="Bad" />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+export default App;
